52a73703cc774467c42b4019f79c73e1019cb56c
Raivenz20 <venzcollege2023@gmail.com>
2024-12-05 12:32:18 +0800

    Sir Dajay Final Activity for First Sem

--- src/Lopez/Record.java
+++ src/Lopez/Record.java
@@ -41,7 +41,7 @@ public class Record {
         br.View();
     }
     private void Specific(){
-        int id;
+        int id, id2;
         boolean exit = true;
         while(exit){
             boolean exit2 = true;
@@ -100,7 +100,7 @@ public class Record {
                 
                 System.out.println("+----------------------------------------------------------------------+");
                 System.out.printf("|%-22s%-25s%-23s|\n", "", "==--Violation History--==", "");
-                String tbl_view = "SELECT Incident_Types.i_name, Incident_Types.i_description, "
+                /*String tbl_view = "SELECT Incident_Types.i_name, Incident_Types.i_description, "
                         + "Blotter_Report.r_fname, Blotter_Report.r_lname, Blotter_Report.r_status, Blotter_Report.r_location, "
                         + "(Blotter_Report.r_fname ||' '|| Blotter_Report.r_lname) AS RepFullname, "
                         + "Blotter_Report.r_incident_date, Blotter_Report.r_reported_date, Blotter_Report.r_solution, Blotter_Report.r_id, "
@@ -115,6 +115,62 @@ public class Record {
                 String[] tbl_Headers = {"Report ID", "Reporter Name", "Officer Name", "Status", "Report Name", "Incident Location", "Incident Date", "Reported Date", "Solution"};
                 String[] tbl_Columns = {"r_id", "RepFullname", "OffFullname", "r_status", "i_name", "r_location", "r_incident_date", "r_reported_date", "r_solution"};
                 conf.viewRecords(tbl_view, tbl_Headers, tbl_Columns);
+                */
+                String tbl_view = "SELECT Incident_Types.i_name,"
+                        + " Blotter_Report.r_status, Blotter_Report.r_id,"
+                        + " (Officer_List.o_fname ||' '|| Officer_List.o_lname) AS OffFullname"
+                        + " From Blotter_Report "
+                        + " INNER JOIN Citizen_List ON Blotter_Report.c_id = Citizen_List.c_id"
+                        + " INNER JOIN Incident_Types ON Blotter_Report.i_id = Incident_Types.i_id"
+                        + " INNER JOIN Officer_List ON Blotter_Report.o_id = Officer_List.o_id"
+                        + " Where Citizen_List.c_id = "+id;
+                String[] tbl_Headers = {"Report ID", "Incident Name", "Status", "Officer Name"};
+                String[] tbl_Columns = {"r_id", "i_name", "r_status", "OffFullname"};
+                conf.viewRecords(tbl_view, tbl_Headers, tbl_Columns);
+                boolean exit3 = true;
+                while(true){
+                    System.out.printf("|%-5sEnter ID of Specific Information (0 to exit): ","");
+                    try{
+                        id2 = input.nextInt();
+                        String SQL = "SELECT COUNT (*) FROM Blotter_Report Where r_id = ? And c_id = "+id;
+                        if(conf.recordExists(SQL, id2)){
+                            break;
+                        }else if(id2 == 0){
+                            exit3 = false;
+                            break;
+                        }else{
+                            System.out.printf("|%-2s%-45s%-3s|\n", "", "!!Id Does Not Match to Existing Information!!", "");
+                        }
+                    }catch(Exception e){
+                        System.out.printf("|%-12s%-25s%-13s|\n", "", "!!Enter A Valid Integer!!", "");
+                        input.nextLine();
+                    }
+                }
+                while(exit3){
+                    String r_fname = "SELECT r_fname FROM Blotter_Report Where r_id = ?";
+                    String r_fname2 = conf.reportVariables(r_fname, id2);
+                    String r_lname = "SELECT r_lname FROM Blotter_Report Where r_id = ?";
+                    String r_lname2 = conf.reportVariables(r_lname, id2);
+                    String r_location = "SELECT r_location FROM Blotter_Report Where r_id = ?";
+                    String r_location2 = conf.reportVariables(r_location, id2);
+                    String r_status = "SELECT r_status FROM Blotter_Report Where r_id = ?";
+                    String r_status2 = conf.reportVariables(r_status, id2);
+                    String r_incident_date = "SELECT r_incident_date FROM Blotter_Report Where r_id = ?";
+                    String r_incident_date2 = conf.reportVariables(r_incident_date, id2);
+                    String r_reported_date = "SELECT r_reported_date FROM Blotter_Report Where r_id = ?";
+                    String r_reported_date2 = conf.reportVariables(r_reported_date, id2);
+                    System.out.println("+----------------------------------------------------------------------+");
+                    System.out.printf("|%-18s%-34s%-18s|\n", "", "==--Full Information Of Report--==", "");
+                    System.out.println("+----------------------------------------------------------------------+");
+                    System.out.printf("|%-5s%-20s%-1s%-2s%-34s%-8s|\n", "", "Reporter First Name", ":", "", r_fname2, "");
+                    System.out.printf("|%-5s%-20s%-1s%-2s%-34s%-8s|\n", "", "Reporter Last Name", ":", "", r_lname2, "");
+                    System.out.printf("|%-5s%-20s%-1s%-2s%-34s%-8s|\n", "", "Location", ":", "", r_location2, "");
+                    System.out.printf("|%-5s%-20s%-1s%-2s%-34s%-8s|\n", "", "Status", ":", "", r_status2, "");
+                    System.out.printf("|%-5s%-20s%-1s%-2s%-34s%-8s|\n", "", "Incident Date", ":", "", r_incident_date2, "");
+                    System.out.printf("|%-5s%-20s%-1s%-2s%-34s%-8s|\n", "", "Reported Date", ":", "", r_reported_date2, "");
+                    System.out.println("+----------------------------------------------------------------------+");
+                    exit3 = false;
+                }
                 exit2 = false;
                 exit = false;
             }
